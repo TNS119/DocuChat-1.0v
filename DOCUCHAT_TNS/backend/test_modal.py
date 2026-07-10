86fa6d0c028ee84ee59cb877d4c3fd56cb31965b
Nikhilendar Sai <nikhilendarsai119@gmail.com>
2026-07-10 23:07:38 +0530

    made Deployment changes

--- DOCUCHAT_TNS/backend/test_modal.py
+++
@@ -1,8 +0,0 @@
-from services.modal_service import extract_documents
-
-with open("sample.pdf", "rb") as f:
-    docs = extract_documents(f.read())
-
-print(f"Received {len(docs)} documents")
-
-print(docs[0])
